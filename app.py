27ba13acd6b2a7b2baea193eab67145fff0fc9f5
Clay Buxton <clay@clbx.io>
2020-07-18 21:57:03 -0400

    server makes and receives requests

--- app.py
+++ app.py
@@ -1,42 +1,45 @@
 import flask
+import mysql.connector
 from flask import request
-import sqlite3
-from sqlite3 import Error
+import config as cfg
 
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
-conn = sqlite3.connect(r"messages.db")
+mydb = mysql.connector.connect(
+    host="localhost",
+    user=cfg.sqlCreds["user"],
+    password=cfg.sqlCreds["pass"],
+    database="LoveCube"
+)
 
-def getNewestMessage(id):
-    query = ("SELECT Message FROM Messages WHERE ID is " + id + "LIMIT 1")
-    cursor = conn.cursor()
-    cursor.execute(query)
-    conn.commit(0)
-    message = cursor.fetchall()
-    print(message)
-    return message
-    
+cur = mydb.cursor()
+cur.execute("USE LoveCube")
+target = "1"
 
-def setNewMessage(newMsg, id):
-    cursor = conn.cursor()
-    cursor.execute(query)
-    conn.commmit(0)
-
-
+def getNewestMessage(target):
+    print("SELECT Message FROM Messages WHERE TARGET = " + target)
+    cur.execute("SELECT Message FROM Messages WHERE TARGET = " + str(target) + " ORDER BY TIMESTAMP DESC LIMIT 1;")
+    result = cur.fetchall()
+    return result[0][0]    
 
 @app.route('/', methods=['GET'])
 def home():
     return "LoveCube"
 
-
 @app.route('/api/newMessage', methods=['POST'])
+def setMessage():
+    token = request.args["token"]
+    message = request.args["message"]
+    cur.execute("INSERT INTO Messages (TARGET,MESSAGE,TIMESTAMP) VALUES (" + str(token) +","+ str(message) + ",NOW());")
+    mydb.commit()
+    return "Added Message"
 
 @app.route('/api/getMessage', methods=['GET'])
 def getMessage():
     if "token" in request.args:
         token = request.args['token']
-        getNewestMessage(token)
+        return getNewestMessage(token)
 
 if __name__ == '__main__':
     app.run()
